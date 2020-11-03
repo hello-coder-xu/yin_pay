40ee1c948bd2d213565dd82f629ccda8967882cf
尹旭 <yin_8023@addcn.com>
2020-11-03 15:35:43 +0800

    调整部分代码

--- android/src/main/java/com/addcn/yin_pay/AlipayUtil.java
+++ android/src/main/java/com/addcn/yin_pay/AlipayUtil.java
@@ -23,17 +23,12 @@ public class AlipayUtil {
         this.context = context;
     }
 
-    @SuppressLint("HandlerLeak")
     private Handler mHandler = new Handler() {
-        @SuppressWarnings("unused")
         public void handleMessage(Message msg) {
             switch (msg.what) {
                 case SDK_PAY_FLAG: {
-                    @SuppressWarnings("unchecked")
-                    PayResult payResult = new PayResult((Map<String, String>) msg.obj);
-                    /**
-                     * 对于支付结果，请商户依赖服务端的异步通知结果。同步通知结果，仅作为支付结束的通知。
-                     */
+                    Map<String, String> resultValue = (Map<String, String>) msg.obj;
+                    PayResult payResult = new PayResult(resultValue);
                     String resultInfo = payResult.getResult();// 同步返回需要验证的信息
                     String resultStatus = payResult.getResultStatus();
                     // 判断resultStatus 为9000则代表支付成功
@@ -41,18 +36,17 @@ public class AlipayUtil {
                         // 该笔订单是否真实支付成功，需要依赖服务端的异步通知。
                     } else {
                         // 该笔订单真实的支付结果，需要依赖服务端的异步通知。
-
                     }
-                    System.out.println("yin_pay alipay result =" + payResult);
-                    result.success(payResult);
+                    String resultString = payResult.toJson();
+                    System.out.println("yin_pay alipay result =" + resultString);
+                    result.success(resultString);
                     break;
                 }
                 default:
+                    result.notImplemented();
                     break;
             }
         }
-
-        ;
     };
 
 
