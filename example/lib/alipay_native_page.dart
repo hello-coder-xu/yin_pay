dd459ee3efb4425191e9bda52d1a90a44254f17c
10457 <yin_8023@addcn.com>
2021-12-29 19:01:46 +0800

    null-safety

--- example/lib/alipay_native_page.dart
+++ example/lib/alipay_native_page.dart
@@ -12,7 +12,7 @@ class AlipayNativePageState extends State<AlipayNativePage> {
     "_input_charset=\"utf-8\"&body=\"203062474003\"&currency=\"HKD\"&forex_biz=\"FP\"&notify_url=\"http://merchants.wantu.cn/pay/notify\"&out_trade_no=\"100020201102112034055143\"&partner=\"2088031755946862\"&payment_inst=\"ALIPAYHK\"&payment_type=\"1\"&product_code=\"NEW_WAP_OVERSEAS_SELLER\"&secondary_merchant_id=\"3023\"&secondary_merchant_industry=\"5311\"&secondary_merchant_name=\"ADDCN TECHNOLOGY (HK) CO., LIMITED 商城\"&seller_id=\"2088031755946862\"&service=\"mobile.securitypay.pay\"&subject=\"203062474003\"&total_fee=\"12\"&sign=\"CgPCwcB2Iyq9U6Osvk0A7jvBTmnvfWbEc7zCWUG4c9IE9GWxqSqpS%2BR1n07d24IToGZ09TtOW5bfty3xSzir75cLsvOcD7kwtBpDSNL4J9WH7yuJEbnMYBRUgmNg2%2FB2KirIJbk6ypEw%2BO74ID0%2FBDhzfFgsAm7j%2F8QS%2BURzfLE%3D\"&sign_type=\"RSA\"",
     "_input_charset=\"utf-8\"&body=\"203066194792\"&currency=\"HKD\"&forex_biz=\"FP\"&notify_url=\"http://merchants.wantu.cn/pay/notify\"&out_trade_no=\"100020201102112927074430\"&partner=\"2088031755946862\"&payment_inst=\"ALIPAYCN\"&payment_type=\"1\"&product_code=\"NEW_WAP_OVERSEAS_SELLER\"&secondary_merchant_id=\"3023\"&secondary_merchant_industry=\"5311\"&secondary_merchant_name=\"ADDCN TECHNOLOGY (HK) CO., LIMITED 商城\"&seller_id=\"2088031755946862\"&service=\"mobile.securitypay.pay\"&subject=\"203066194792\"&total_fee=\"11\"&sign=\"AbOiRtB8%2B4hFUBMrZzfz9jPfUzu3Ecqqb35dkrZPKPGpLklUUOShfloeh7lanKTZ4MEJFlIS0AZhmq8eA3ieI%2BjBdZmd7QMmI0Vb5tGTRIOxM5NOn0UmhULWvM3VTELC%2BJ9kYNIbYoqfUnTkRtO8FsiZ%2BQOqKP5Y9nNkAG9Wql4%3D\"&sign_type=\"RSA\""
   ];
-  String orderInfo;
+  late String orderInfo;
   bool isHk = true;
   String result = 'no result';
 
